ebba6743d1e581fcacd8feefd3d0f01294e9fa8d
Noor <74349478+bsnoor@users.noreply.github.com>
2022-12-22 18:11:42 +0200

    Update Ex8.m

--- Ex8.m
+++ Ex8.m
@@ -21,7 +21,7 @@ elseif (logical(m24 < m14) && logical(m24 < m34))
 else
     fprintf('the most close point is :m3\n');
 end
-
+disp(" ");
 disp("Q2");
 x1=input("enter the first coordinate of  p1");
 y1=input("enter the second coordinate of p1");
@@ -58,6 +58,7 @@ x3= input("enter third");
 x4= input("enter fourth");
 sum3=0;
 multi=1;
+M=[x1 x2 x3 x4];
 if (1 <= x1 && x1 <= 5) && (1 <= x2 && x2 <= 5) &&... 
     (1 <= x3 && x3 <= 5)  && (1 <= x4 && x4 <= 5)
     result=true;
@@ -69,12 +70,12 @@ if result==true
       sum3=sum3+M(i);
       multi=multi*M(i);
    end
-   fprintf('the sum is %d \n:',sum)
+   fprintf('the sum is %d \n:',sum3)
    fprintf('the multi is %d:',multi)
 else
     disp("the numbers not in the range")
 end
-
+disp(" ");
 disp("Q4");
 x=input("enter x");
 x1= input("enter fisrt");
@@ -94,7 +95,7 @@ elseif(x>=x3 && x<x4)
 elseif(x>=x4)
     fprintf('x elemnt of N5')
 end
-
+disp(" ");
 disp("Q5");
 x1= input("enter fisrt");
 x2= input("enter second");
@@ -113,12 +114,12 @@ if result==true
       sum3=sum3+M(i);
       multi=multi*M(i);
    end
-   fprintf('the sum is %d \n:',sum)
+   fprintf('the sum is %d \n:',sum3)
    fprintf('the multi is %d:',multi)
 else
     disp("the numbers not in the range")
 end
-
+disp(" ");
 disp("Q6");
 x1= input("enter fisrt");
 x2= input("enter second");
@@ -130,7 +131,7 @@ v_sorted = sort(v);
 disp(['the first element is ', num2str(v_sorted(1))])
 disp(['the second element is ', num2str(v_sorted(2))])
 disp(['the third element is ', num2str(v_sorted(3))])
-
+disp(" ");
 disp("Q7");
 x1= input("enter fisrt");
 x2= input("enter second");
@@ -143,15 +144,15 @@ v_sorted = sort(v);
 disp(['the max element is ', num2str(v_sorted(4))])
 disp(['the min element is ', num2str(v_sorted(1))])
 
-
+disp(" ");
 disp("Q8")
 x1= input("enter fisrt");
 x2= input("enter second");
 x3= input("enter third");
 if(x1+x2>x3 && x1+x3>x2 && x3+x2>x1)
-    if(x1==x2||x2==x3||x1==x3)
+    if(x1==x2 && x2==x3)
         disp(' two sides is equal')
-    elseif(x1==x2 && x2==x3)
+    elseif(x1==x2||x2==x3||x1==x3)
         disp('all sides is equal ')
     else
         disp('isnt equal')
@@ -160,7 +161,7 @@ else
   disp('isnt triangle')
 end
 
-
+disp(" ");
 disp("Q10");
 x1= input("enter fisrt");
 x2= input("enter second");
@@ -176,7 +177,7 @@ for i=1:3
    end
 end
 fprintf('max is %d', max )
-
+disp(" ")
 disp("Q11 + Q12");
 x1= input("enter a x1: ");
 x2= input("enter a x2: ");
@@ -195,7 +196,7 @@ end
 
 fprintf('sum in even place is %d \n', sum1 )
 fprintf('sum in odd place is %d \n', sum2 )
-
+disp("");
 disp("Q13")
 A=randi([-10,10],1,5);
 disp(A);
@@ -211,7 +212,7 @@ if(count>0)
 else
     disp("0")
 end
-
+disp("");
 disp("Q15")
 A=randi([-10,10],1,5);
 disp(A);
