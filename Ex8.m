7c8783df7d70cc71d9fd23aa18c4557e6a7a8a50
Noor <74349478+bsnoor@users.noreply.github.com>
2022-12-22 11:59:32 +0200

    Update Ex8.m

--- Ex8.m
+++ Ex8.m
@@ -1,4 +1,5 @@
-#Q1
+clc, clear
+disp("Q1");
 x1 = input("enter a M1x: ");
 y1 = input("enter a M1y: ");
 x2 = input("enter a M2x: ");
@@ -8,18 +9,14 @@ y3 = input("enter a M3y: ");
 x4 = input("enter a M4x: ");
 y4 = input("enter a M4y: ");
 
-m1 = [x1 y1];
-m2 = [x2 y2];
-m3 = [x3 y3];
-m4 = [x4 y4];
 
-m14 = m4 - m1;
-m24 = m4 - m2;
-m34 = m4 - m3;
+m14 = sqrt((x4-x1)^2+(y4-y1)^2);
+m24 = sqrt((x4-x2)^2+(y4-y2)^2);
+m34 =sqrt((x4-x3)^2+(y4-y3)^2);
 
-if m14 > m24 & m14 > m34
+if (logical(m14 < m24) && logical(m14 < m34))
     fprintf('the most close point is : m1\n');
-elseif m24 > m14 & m24 > m34
+elseif (logical(m24 < m14) && logical(m24 < m34))
     fprintf('the most close point is : m2\n');
 else
     fprintf('the most close point is :m3\n');
