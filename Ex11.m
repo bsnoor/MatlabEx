e12013b0b2c8b1e7bdef0f873beb5b66e5452163
Noor <74349478+bsnoor@users.noreply.github.com>
2023-01-02 19:30:13 +0200

    Update Ex11.m

--- Ex11.m
+++ Ex11.m
@@ -33,5 +33,21 @@ function queue(V)
   disp(shift2);
 end
 
+disp("Q3");
+v=[2 4 5 6 2 8 2 5 1];
+id(v);
+function id(uID)
+  hw=10*uID(2)+10;
+  disp("hw:")
+  disp(hw);
+  midtest=5*(uID(5)+uID(9));
+  disp("midtest:");
+  disp(midtest);
+  fintest=1.67*(uID(2)+uID(3))+3.5*(uID(4)+uID(5)+2);
+    disp("fintest:");
+  disp(fintest)
+
+ end
+
 
 
