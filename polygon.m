b259c6a62622a1526d096450b74d9ae23543bb0c
Noor <74349478+bsnoor@users.noreply.github.com>
2023-01-08 18:26:59 +0200

    Update polygon.m

--- polygon.m
+++ polygon.m
@@ -1,9 +1,10 @@
 function S=polygon(A ,B,C,D,E)
+function S=polygon(A ,B,C,D,E)
 
-AE=[E(1)-A(1),E(2)-A(2),E(3)-A(3)];
-AD=[D(1)-A(1),D(2)-A(2),E(3)-A(3)];
+AE=[E(1)-A(1),E(2)-A(2),0];
+AD=[D(1)-A(1),D(2)-A(2),0];
 
-BD=[B(1)-D(1),B(2)-D(2),E(3)-A(3)];
+BD=[B(1)-D(1),B(2)-D(2),0];
 
 BC=[C(1)-B(1),C(2)-B(2),E(3)-A(3)];
 a=abs(cross(AE,AD)/2);
@@ -13,3 +14,4 @@ S=abs(a(3)+b(3)+c(3));
 disp("the area is equal:")
 disp(S);
 end
+
