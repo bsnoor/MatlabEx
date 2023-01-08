bbf956bff0c07749304d98d05fb825e80d723cab
Noor <74349478+bsnoor@users.noreply.github.com>
2023-01-08 13:05:01 +0200

    Update polygon.m

--- polygon.m
+++ polygon.m
@@ -6,10 +6,10 @@ AD=[D(1)-A(1),D(2)-A(2),E(3)-A(3)];
 BD=[B(1)-D(1),B(2)-D(2),E(3)-A(3)];
 
 BC=[C(1)-B(1),C(2)-B(2),E(3)-A(3)];
-a=abs(dot(AE,AD)/2);
-b=abs(dot(AD,BD)/2);
-c=abs(dot(BD,BC)/2);
-S=abs(a+b+c);
-disp("the area :")
+a=abs(cross(AE,AD)/2);
+b=abs(cross(AD,BD)/2);
+c=abs(cross(BD,BC)/2);
+S=abs(a(3)+b(3)+c(3));
+disp("the area is equal:")
 disp(S);
 end
