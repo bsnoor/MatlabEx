72bd5c6e94b5791589102412ca553c406d112c8e
Noor <74349478+bsnoor@users.noreply.github.com>
2023-01-08 13:58:25 +0200

    Update Ex11.m

--- Ex11.m
+++ Ex11.m
@@ -72,3 +72,40 @@ function triangle(ID)
     disp(A);
 end 
 
+
+disp("Q5")
+a=[-4 3];
+b=[-2 -3];
+c=[5 -3];
+o=[0 0];
+a1=[-10 0];
+b2=[10 0];
+a2=[0 -6];
+b1=[0 8];
+b3=[0 4];
+hold on
+axis equal
+grid on
+plot(a(1),a(2),'.b','MarkerSize',10)
+plot(b(1),b(2),'.b','MarkerSize',10)
+plot(c(1),c(2),'.b','MarkerSize',10)
+text(-5,4,'A(-4,3)')
+text(-2,-3,'B(-2,-3)')
+text(5,-3,'C(5,-3)')
+text(-5,-1,'P3=25')
+text(-1,-2,'P3')
+text(5,-1,'P4=50')
+text(1,-2,'P4')
+text(3,3,'P1=10')
+text(0,3,'P1')
+text(-5,1,'P2=20')
+text(-3,3,'P2')
+quiver(o(1),o(2),a(1)-o(1),a(2)-o(2),1,'b','Linewidth',1)
+quiver(o(1),o(2),c(1)-o(1),c(2)-o(2),1,'b','Linewidth',1)
+quiver(o(1),o(2),b(1)-o(1),b(2)-o(2),1,'b','Linewidth',1)
+quiver(o(1),o(2),b(1)-o(1),b(2)-o(2),1,'b','Linewidth',1)
+quiver(a1(1),a1(2),b2(1)-a1(1),b2(2)-a1(2),1,'b','Linewidth',1)
+text(13,1,'x','FontSize',14)
+quiver(a2(1),a2(2),b1(1)-a2(1),b1(2)-a2(2),1,'b','Linewidth',1)
+text(1,13,'y','FontSize',14)
+quiver(o(1),o(2),b3(1)-o(1),b3(2)-o(2),1,'b','Linewidth',2)
